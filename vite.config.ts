ea8f7b0c1a037d3dfb64bbf4be3c2cd525460c8f
Aktishch <aktishch@gmail.com>
2026-07-24 18:45:05 +0300

    update vite for app js

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from '@tailwindcss/vite'
 import nunjucks from '@vituum/vite-plugin-nunjucks'
+import fs from 'node:fs'
+import path from 'node:path'
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, UserConfig } from 'vite'
 import checker from 'vite-plugin-checker'
-import { ViteMinifyPlugin } from 'vite-plugin-minify'
 import vituum from 'vituum'
 
 const resolvePath = (dir: string) => {
@@ -21,11 +22,13 @@ export default defineConfig({
     }
   },
   plugins: [
-    ViteMinifyPlugin({
-      removeComments: true,
-      collapseWhitespace: false
+    vituum({
+      imports: {
+        filenamePattern: {
+          '+.js': []
+        }
+      }
     }),
-    vituum(),
     nunjucks(),
     tailwindcss(),
     checker({
@@ -37,14 +40,36 @@ export default defineConfig({
       stylelint: {
         lintCommand: 'stylelint "./src/**/*.css"'
       }
-    })
+    }),
+    {
+      name: 'vite-clean-js-garbage',
+      closeBundle() {
+        const distJs = resolvePath('./dist/js')
+
+        if (fs.existsSync(distJs)) {
+          fs.readdirSync(distJs)
+            .filter((file) => {
+              return !file.startsWith('application.')
+            })
+            .forEach((file) => {
+              fs.unlinkSync(path.join(distJs, file))
+            })
+        }
+      }
+    }
   ],
   build: {
     cssCodeSplit: false,
+    sourcemap: true,
     rollupOptions: {
       output: {
+        entryFileNames: 'js/[name].js',
         chunkFileNames: 'js/application.js',
-        entryFileNames: 'js/application.js',
+        manualChunks(id) {
+          if (id.includes('src/scripts') || id.includes('node_modules') || id.endsWith('.ts') || id.endsWith('.js')) {
+            return 'application'
+          }
+        },
         assetFileNames: (asset) => {
           return asset.names?.some((name) => {
             return name.endsWith('.css')
